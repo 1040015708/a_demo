11e3ced6a62d8b9cd13237ef465106fe3b96969f
Jin <1040015708@qq.com>
2022-08-19 16:59:04 +0800

    hot-fix first commit

--- src/main/java/com/example/demo/a/Test.java
+++ src/main/java/com/example/demo/a/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(Math.round(-1.4));
         System.out.println("git");
+        System.out.println("hot-fix");
     }
 }
